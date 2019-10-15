15b15d28fb31e592bef1d82f5ef60c9ce30329fa
Sar-dim <55599642+Sar-dim@users.noreply.github.com>
2019-10-15 10:30:36 +0300

    Files of create game board and ships

---
+++ ioarray.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef IOARRAY
+#define IOARRAY
+
+void CreateBoard(int** gameBoard, int size);
+void OutputBoard(int** gameBoard, int size);
+void InitBoard(int** gameBoard, int size);
+
+#endif
