38c27405c4203a655708f76aebbf24e176d889bf
Sar-dim <55599642+Sar-dim@users.noreply.github.com>
2019-10-16 20:35:33 +0300

    Work version with arrangment by arrows

--- SeaBattle.cpp
+++ SeaBattle.cpp
@@ -40,7 +40,7 @@ int main()
 	cout << "Let arrange first player ships " << endl;
 	InitBoardPrototype(gameBoard1, gameBoardTemp, SIZE);
 	cout << "Let arrange second player ships " << endl;
-	InitBoard(gameBoard2, SIZE);
+	InitBoardPrototype(gameBoard1, gameBoardTemp, SIZE);
 	system("cls");
 	cout << "First player board " << endl;
 	OutputBoard(gameBoard1, SIZE);
