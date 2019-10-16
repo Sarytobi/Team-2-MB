106ace87dce1306a30e049e978ba17565f92cbb3
Sar-dim <55599642+Sar-dim@users.noreply.github.com>
2019-10-16 23:08:05 +0300

    Work version with arrangment by arrows

--- ioarray.h
+++ ioarray.h
@@ -5,7 +5,6 @@
 void CreateBoard(int** gameBoard, int size);
 void OutputBoard(int** gameBoard, int size);
 void OutputBoardDisguise(int** gameBoard, int size);
-void InitBoard(int** gameBoard, int size);
 void InitBoardPrototype(int** gameBoard, int** gameBoardTemp, int size);
 void OutputBoardPrototype(int** gameBoard, int** gameBoardTemp, int size, char shipDirection, int startPointRow, int startPointCol, int value);
 #endif
